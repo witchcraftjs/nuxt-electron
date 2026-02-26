75f2e63c26411fae437654ba57184708a5c69a02
Alan North <alanscodelog@gmail.com>
2026-02-26 12:48:48 -0300

    v0feat: more improvements to route proxies
    - Only the longest key matched will be used instead of the first.
    - You can now handle the path conversion/concactenation yourself.
    - Fixed example with wrong proxy path.
    - Added ignore option to ignore a sub-route.
    - Improved caching, proxy routes are now also cached.
    - Caches are now exported if you want to manually clear them.

--- src/runtime/electron/createProxiedProtocolHandler.ts
+++ src/runtime/electron/createProxiedProtocolHandler.ts
@@ -7,35 +7,24 @@ import path from "node:path"
 import { pathToFileURL } from "node:url"
 
 
-const cache = new Map<string, string | undefined>()
+export const cache = new Map<string, string | undefined>()
+export const proxyCache = new Map<string, string | undefined>()
+const proxiedProperties: (keyof Request)[] = ["headers", "destination", "referrer", "referrerPolicy", "mode", "credentials", "cache", "redirect", "integrity", "keepalive"]
 
 async function getPathToServe(
 	originalPath: string,
 	pathWithIndexHtml: string,
-	/** Usually the request url */
-	key: string,
+	requestUrl: string,
 	logger?: {
 		trace: (...args: any[]) => void
 	}
 ) {
-	if (cache.has(key)) {
-		const result = cache.get(key)
-		if (logger) {
-			logger.trace({
-				ns: "main:createProxiedProtocolHandler:cacheHit",
-				requestUrl: key,
-				result: cache.get(key)
-			})
-		}
-		return result
-	}
 	const stats = await fs.stat(originalPath).catch(() => undefined)
-
 
 	if (logger) {
 		logger.trace({
 			ns: "main:createProxiedProtocolHandler:stats",
-			key,
+			requestUrl,
 			exists: !!stats,
 			isFile: stats?.isFile(),
 			isDirectory: stats?.isDirectory()
@@ -51,9 +40,28 @@ async function getPathToServe(
 						: undefined
 				: undefined
 		: undefined
-	cache.set(key, pathToServe)
 	return pathToServe
 }
+
+async function getFromCacheOr(cache: Map<string, string | undefined>, key: string, fn: (key: string) => Promise<string | undefined> | string | undefined, logger?: {	trace: (...args: any[]) => void }): Promise<string | undefined> {
+	let result = cache.get(key)
+	if (result) {
+		if (logger) {
+			logger.trace({
+				ns: "main:createProxiedProtocolHandler:cacheHit",
+				requestUrl: key,
+				result
+			})
+		}
+		return result
+	}
+	result = await fn(key)
+	if (result) {
+		cache.set(key, result)
+	}
+	return result
+}
+
 export function createProxiedProtocolHandler(
 	/** `protocol` or `session.protocol` depending on if you're using paritions with your windows or not, regular `protocol` only registers the handler for the default parition. */
 	protocol: Protocol,
@@ -64,14 +72,38 @@ export function createProxiedProtocolHandler(
 	 *
 	 * At least `/api` should be added for a basic nuxt app to work correctly.
 	 *
+	 * The longest key matched will be used.
 	 *
 	 * ```ts
 	 * routeProxies: {
-	 * 	"/api": "http://localhost:3000/api",
+	 * 	"/api": "http://localhost:3000", // turns into http://localhost:3000/api
+	 * 	// for more control, for example: to proxy /your/path to some other path while removing the /your/path part:
+	 * 	"/your/path": {
+	 * 		url: "http://some-site.com",
+	 * 		convertPath: (proxyKey, url, requestPath) => url + requestPath.slice(proxyKey.length)
+	 * 	}
+	 * 	// to ignore a path
+	 * 	"/ignore/some/path": {
+	 * 		ignore: true
+	 * 	}
 	 * }
 	 * ```
 	 */
-	routeProxies: Record<string, string>,
+	routeProxies: Record<
+		string,
+		| string
+		| {
+			url: string
+			/** Override how the path is merged with the url. The default literally just concatenates url + path. */
+			convertPath?: (proxyKey: string, url: string, requestPath: string) => string
+			ignore?: false
+		}
+		|	{
+			url: never
+			convertPath: never
+			ignore: true
+		}
+	>,
 	{
 		logger,
 		errorPage = "404.html"
@@ -108,7 +140,7 @@ export function createProxiedProtocolHandler(
 	// to check the user correctly registered it
 	// it's deprecated for some reason and also it doesn't work (so maybe because of that)!
 	protocol.handle(protocolName, async request => {
-		errorPage404 ??= await getPathToServe(path.join(basePath, errorPage), "404.html", "404.html", logger)
+		errorPage404 ??= await getFromCacheOr(cache, "404.html", key => getPathToServe(path.join(basePath, errorPage), "404.html", key, logger), logger)
 
 		if (!errorPage404) {
 			throw new Error(`Error page ${path.join(basePath, errorPage)} does not exist. Did you override the routeRules for it?`)
@@ -118,23 +150,45 @@ export function createProxiedProtocolHandler(
 		// we can ignore the host, as getPaths sets it to bundle (e.g. protocol://bundle/path/to/file)
 		const requestPath = decodeURIComponent(parsedUrl.pathname)
 
-		const proxyKey = routeProxyKeys.find(key => requestPath.startsWith(key))
 
-		if (proxyKey) {
-			const proxyUrl = routeProxies[proxyKey]
-			const finalPath = proxyUrl + requestPath
+		// this must be on a seperate cache because the result and key are different since fetching is different
+		const cachedProxyResult = await getFromCacheOr(proxyCache, requestPath, _ /* requestPath */ => {
+			const matchedProxyKeys = routeProxyKeys.filter(key => requestPath.startsWith(key))
+			const proxyKey = matchedProxyKeys.sort((a, b) => b.length - a.length)[0]
+
+			const proxyEntry = proxyKey && routeProxies[proxyKey]
+			if (proxyKey && proxyEntry && !(typeof proxyEntry === "object" && proxyEntry?.ignore)) {
+				const proxyUrl = typeof proxyEntry === "string" ? proxyEntry : proxyEntry?.url
+
+				const finalPath = typeof proxyEntry === "object" && proxyEntry?.convertPath
+					? proxyEntry.convertPath(proxyKey, proxyUrl, requestPath)
+					: proxyUrl + requestPath
+
+				if (logger) {
+					logger.trace({
+						ns: "main:createProxiedProtocolHandler:fetchingViaProxy",
+						requestUrl: request.url,
+						proxyKey,
+						route: proxyUrl,
+						requestPath,
+						finalPath
+					})
+				}
+				return finalPath
+			}
+			return undefined
+		})
+		if (cachedProxyResult) {
 			if (logger) {
 				logger.trace({
-					ns: "main:createProxiedProtocolHandler:fetchingViaProxy",
+					ns: "main:createProxiedProtocolHandler:fetchingViaProxy(cached)",
 					requestUrl: request.url,
-					proxyKey,
-					route: proxyUrl,
 					requestPath,
-					finalPath
+					finalPath: cachedProxyResult
 				})
 			}
-			return net.fetch(finalPath, {
-				...pick(request, ["headers", "destination", "referrer", "referrerPolicy", "mode", "credentials", "cache", "redirect", "integrity", "keepalive"])
+			return net.fetch(cachedProxyResult, {
+				...pick(request, proxiedProperties)
 			})
 		}
 
@@ -173,7 +227,7 @@ export function createProxiedProtocolHandler(
 		}
 
 
-		const pathToServe = await getPathToServe(originalPath, pathWithIndexHtml, request.url, logger)
+		const pathToServe = await getFromCacheOr(cache, request.url, key => getPathToServe(originalPath, pathWithIndexHtml, key, logger), logger)
 
 		if (!pathToServe) {
 			if (logger) {
