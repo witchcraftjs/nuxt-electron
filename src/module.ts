71fdbc9ae664a2abcea3de734dc39f96e535b135
Alan North <alanscodelog@gmail.com>
2026-06-01 22:59:52 -0300

    deps: updated to latest @witchcraft/ui

--- src/module.ts
+++ src/module.ts
@@ -174,7 +174,7 @@ export default defineNuxtModule<ModuleOptions>({
 	},
 	moduleDependencies: {
 		"@witchcraft/ui/nuxt": {
-			version: "^0.4.0"
+			version: "^0.5.0"
 		}
 	},
 	async setup(options, nuxt) {
